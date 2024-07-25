8ade2897de1d2ae82815731d97ff2383bba3f0e0
Dipanshu Mandal <dipanshumandal@Dipanshus-MacBook-Air.local>
2024-07-25 22:39:20 +0530

    second commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-
+import HeroSection from "../components/Herosection";
 const home = () => {
   return (
-    <>
-    <Navbar/>
-    <div>home</div>
-    </>
+    <div className='mt-36'>
+    <HeroSection />
+    </div>
   )
 }
 
