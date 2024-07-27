957a0c147179d4d6c9ac3f5baa5d9b144bb6dde9
Dipanshu Mandal <dipanshumandal@Dipanshus-MacBook-Air.local>
2024-07-27 23:14:50 +0530

    Database Connected

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@
 // Express ko bulana hoga is file mai
 const express=require("express");
 const dotenv=require("dotenv")
+const { connectDb } = require("./connection");
+const {readdirSync} =require("fs");
+const { connect } = require("http2");
+
+//import the route
+//const authRoute=require("./routes/authRoutes")
 
 //Binding env
 dotenv.config();
@@ -13,10 +19,26 @@ const app=express();
 // Port ko define krna parega : Port=Door
 const port=process.env.PORT || 3000;
 
+connectDb()
+
 //Making Routes
 app.get("/",(req,res)=>{
     res.send("Server Running...")
 })
+
+//How to use Routes
+//app.use("/api",authRoute)
+
+//Importing and Using Routes Dynamically
+readdirSync("./routes").map((route)=>{
+    app.use("/api",require(`./routes/${route}`))
+});
+
+// types of requests
+// 1. GET -> To get the data from the server
+// 2. POST -> To post the data to the server
+// 3. PUT -> To update the data on the server
+// 4. DELETE -> To dete the data form the server
 
 // Server ko Listen krna hoga 
 app.listen(port,()=>{
