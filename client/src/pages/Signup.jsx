324faf2af8494584eae0e37946c1ec8a9e139cde
Dipanshu Mandal <dipanshumandal@Dipanshus-MacBook-Air.local>
2024-07-31 12:37:34 +0530

    Connected Signup from Frontend

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,32 +1,79 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+import { useState } from "react"
+import toast from 'react-hot-toast'
 
 const Signup = () => {
+
+  const navigate=useNavigate()
+
+  const [username,setUsername] =useState("")
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
+  const [accountType,setAccounttype]=useState("")
+
+  const handleSignup = async (e) =>{
+    e.preventDefault();
+    try {
+      const res=await axios.post(import.meta.env.VITE_API_URL+"/signup",{
+        username,
+        email,
+        password,
+        accountType
+      })
+      const data =await res.data;
+      if(data.success){
+        setUsername("");
+        setEmail("");
+        setPassword("");
+        setAccounttype("");
+        toast.success(data.message);
+        navigate("/login");
+      }
+    } catch (error) {
+      toast.error(error.response.data.message)
+    }
+  }
+
   return (
     <div className='mt-20 sm:mt-10 min-h-screen flex items-center justify-center w-full'>
       <div className='bg-white shadow-md rounded-3xl px-5 py-6 w-full sm:w-[27vw]'>
       <h1 className='text-2xl font-bold text-center mb-4'>
         Let's Connect!
       </h1>
-      <form>
+      <form onSubmit={handleSignup}>
         {/* For UserName */}
         <div className='mb-4'>
           <label htmlFor='username' className='block text-sm font-medium text-gray-700 mb-2'>Username</label>
-          <input type='text' name='name' id="name" placeholder='Name' className='shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black'/>
+          <input type='text' 
+          name='name' 
+          id="name" 
+          placeholder='Name' 
+          value={username} 
+          onChange={(e)=>setUsername(e.target.value)}
+          className='shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black'/>
         </div>
         {/* For Email */}
         <div className='mb-4'>
           <label htmlFor='email' className='block text-sm font-medium text-gray-700 mb-2'>Email Address</label>
-          <input type='text' name='email' id="email" placeholder='your@email.com' className='shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black'/>
+          <input type='text' name='email' id="email" placeholder='your@email.com' 
+          value={email} 
+          onChange={(e)=>setEmail(e.target.value)}
+          className='shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black'/>
         </div>
         {/* For Password */}
         <div className='mb-4'>
           <label htmlFor='password' className='block text-sm font-medium text-gray-700 mb-2'>Password</label>
-          <input type='password' name='password' id="password" placeholder='Password' className='shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black'/>
+          <input type='password' name='password' id="password" placeholder='Password' 
+          value={password} 
+          onChange={(e)=>setPassword(e.target.value)}
+          className='shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black'/>
         </div>
         {/* Account Type */}
         <div className='mb-4'>
           <label htmlFor='accountType' className='block text-sm font-medium text-gray-700 mb-2'>Select Your Account Type</label>
-           <select className='shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black'>
+           <select onChange={(e)=>setAccounttype(e.target.value)} 
+           className='shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black'>
               <option value="buyer" >Buyer</option>
               <option value="seller">Seller</option>
            </select>
